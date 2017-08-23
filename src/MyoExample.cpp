f1032f7ac1ad925eee31d423382b285d432325b9
Oswaldo <oswaldopena@usantotomas.edu.co>
2017-08-23 16:45:44 -0500

    Add license to sources

--- src/MyoExample.cpp
+++ src/MyoExample.cpp
@@ -1,5 +1,25 @@
-#include "include/MyoExample.h"
+//======================================================================//
+//  This software is free: you can redistribute it and/or modify        //
+//  it under the terms of the GNU General Public License Version 3,     //
+//  as published by the Free Software Foundation.                       //
+//  This software is distributed in the hope that it will be useful,    //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of      //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE..  See the      //
+//  GNU General Public License for more details.                        //
+//  You should have received a copy of the GNU General Public License   //
+//  Version 3 in the file COPYING that came with this distribution.     //
+//  If not, see <http://www.gnu.org/licenses/>                          //
+//======================================================================//
+//                                                                      //
+//      Copyright (c) 2017 Oswaldo Peña and Saith Rodriguez             //
+//      Universidad Santo Tomas - Bogota, Colombia                      //
+//      oswaldopena@usantotomas.edu.co                                  //
+//      saithrodriguez@usantotomas.edu.co                               //
+//                                                                      //
+//======================================================================//
 
+
+#include "include/MyoExample.h"
 
 MyoExample::MyoExample()
 {   
